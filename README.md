191e70a0221d962da6ef671a65bf5e2b09c7db70
Romulohenriquesc <romulohsc7@gmail.com>
2025-03-25 00:31:24 -0300

    First tests

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # tdd-project-reservations
+
+## First step
+
+Start a project
+```
+npm init -y
+```
+
+Install dependencies
+```
+npm install --save-dev jest @types/jest ts-jest typescript
+```
+
+Oooooor only
+```
+npm install
+```
---
+++ src/domain/value_objects/date_range.test.ts
@@ -0,0 +1,38 @@
+import { DateRange } from './date_range';
+
+describe('DateRange Value Object', () => {
+
+    it('Deve lançar um erro se a data de término for antes da data de início', () => {
+        expect(() => {
+            new DateRange(new Date('2024-12-25'), new Date('2024-12-20'));
+        }).toThrow('A data de término deve ser posterior à data de início');
+    });
+
+    it('Deve criar uma instância de DateRange com a data de início e término, e verificar o retorno dessas datas.', () => {
+        const startDate = new Date('2024-12-20');
+        const endDate = new Date('2024-12-25');
+
+        const dateRange = new DateRange(startDate, endDate);
+
+        expect(dateRange.getStartDate()).toEqual(startDate);
+        expect(dateRange.getEndDate()).toEqual(endDate);
+    });
+
+    it('Deve calcular o total de noites corretamente', () => {
+        const startDate = new Date('2024-12-20');
+        const endDate = new Date('2024-12-25');
+
+        const dateRange = new DateRange(startDate, endDate);
+        const totalNights = dateRange.getTotalNights();
+
+        expect(totalNights).toBe(5);
+
+        const startDate1 = new Date('2024-12-10');
+        const endDate1 = new Date('2024-12-11');
+        
+        const dateRange1 = new DateRange(startDate1, endDate1);
+        const totalNights1 = dateRange1.getTotalNights();
+
+        expect(totalNights1).toBe(1);
+    });
+});
