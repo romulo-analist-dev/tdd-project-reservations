30a300537c48f6dd684f5ce39e03a36fb57afc7d
Romulohenriquesc <romulohsc7@gmail.com>
2025-03-25 21:14:39 -0300

    Property partial

---
+++ src/domain/entities/property.test.ts
@@ -0,0 +1,47 @@
+import { Property } from './property';
+import { DateRange } from '../value_objects/date_range';
+
+describe('Property Entity', () => {
+    
+    it('Deve criar uma instância de Property com todos os atributos', () => {
+        
+        const property = new Property(
+            '1',
+            'Casa de praia',
+            'Uma bela casa na praia',
+            4,
+            200);
+
+            expect(property.getId()).toBe('1');
+            expect(property.getName()).toBe('Casa de praia');
+            expect(property.getDescription()).toBe('Uma bela casa na praia');
+            expect(property.getMaxGuests()).toBe(4);
+            expect(property.getBasePricePerNight()).toBe(200);
+        
+    });
+
+    it('Deve lançar um erro se o nome for vazio', () => {
+        
+        expect(() => new Property('1', '', 'Uma bela casa na praia', 4, 200)).toThrow('o nome é obrigatório');
+        
+    });
+
+    it('Deve lançar um erro e o número máximo de hóspedes for zero ou negativo', () => {
+        expect(() => new Property('1', 'Casa de praia', 'Uma bela casa na praia', 0, 200)).toThrow('o número máximo de hóspedes deve ser maior que zero');
+    });
+
+    it('Deve validar o número máximo de hóspedes', () => {
+        const property = new Property('1', 'Casa de praia', 'Uma bela casa na praia', 5, 150);
+
+        expect(() =>{
+            property.validateGuestCount(6);
+        }).toThrow('o número máximo de hóspedes excedudo. Máximo permitido: 5.');
+        
+    });
+
+    it('Não deve aplicar desconto para estadias menores que 7 noites', () => {
+        const property = new Property('2', 'Apartamento na praia', 'AP Show', 2, 100);
+
+        const dateRange = new DateRange();
+    });
+});
---
+++ src/domain/entities/property.ts
@@ -0,0 +1,57 @@
+export class Property{
+
+    private readonly id: string;
+    private readonly name: string;
+    private readonly description: string;
+    private readonly maxGuests: number;
+    private readonly basePricePerNight: number;
+
+    constructor(
+        id: string,
+        name: string,
+        description: string,
+        maxGuests: number,
+        basePricePerNight: number
+    ){
+
+        if (!name) {
+            throw new Error('o nome é obrigatório');
+        }
+
+        if(maxGuests <= 0){
+            throw new Error('o número máximo de hóspedes deve ser maior que zero');
+        }
+
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.maxGuests = maxGuests;
+        this.basePricePerNight = basePricePerNight;
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+
+    getMaxGuests(): number {
+        return this.maxGuests;
+    }
+
+    getBasePricePerNight(): number {
+        return this.basePricePerNight;
+    }
+
+    validateGuestCount(guestCount: number): void {
+        if(guestCount > this.maxGuests){
+            throw new Error(`o número máximo de hóspedes excedudo. Máximo permitido: ${this.maxGuests}.`);
+        }
+    }
+}
